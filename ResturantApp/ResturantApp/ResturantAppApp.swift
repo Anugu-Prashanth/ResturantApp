279266012966c8670b973329a090ca925e70ef79
Anugu-Prashanth <anugu.prashanth423@gmail.com>
2023-06-04 01:18:32 -0500

    init

---
+++ ResturantApp/ResturantApp/ResturantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResturantAppApp.swift
+//  ResturantApp
+//
+//  Created by Anugu Prashanth on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ResturantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
